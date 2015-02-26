2609e5041887a61d858c6654ced2c19328acc019
acreilly <acreilly3@gmail.com>
2015-02-26 16:26:51 -0500

    validation for time unit and time unit length if time unit is greater than time unit length

--- app/models/spree/subscription_interval.rb
+++ app/models/spree/subscription_interval.rb
@@ -1,6 +1,15 @@
 class Spree::SubscriptionInterval < ActiveRecord::Base
   include Spree::Concerns::Intervalable
-
+  before_save :check_time_units
 
   has_and_belongs_to_many :products, :class_name => "Spree::Product", :join_table => 'spree_subscription_intervals_products'
+
+
+
+  def check_time_units
+    if self.time_unit > self.time_unit_length
+      self.errors.add(:time_unit_length,'Needs to be greater than or equal to reorder interval')
+      false
+    end
+  end
 end
